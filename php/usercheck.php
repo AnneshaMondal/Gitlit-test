b47d6e815b9498c64aaf278dfeb178d0cb62e488
AnneshaMondal <amondal@hawk.iit.edu>
2020-04-16 15:45:34 -0500

    adding page for when users enter registry code

--- php/usercheck.php
+++ php/usercheck.php
@@ -2,29 +2,40 @@
 session_start(); // Starting Session
 $error = ''; //varaibles to store the error message
 
-if (isset($_POST['submit'])) {
-if (empty($_post['username']) || empty($_POST['password'])) {
-$error= "Username or Password is invalid";
+if (isset($_POST['Sign In'])) {
+if (empty($_post['email']) || empty($_POST['password'])) {
+$error= "Email or Password is invalid";
 }
 else
 {
 // Define he $username and $password
-$username = $_POST['username'];
+$username = $_POST['email'];
 $password = $_POST['password'];
+$adminkey = $_POST['adminkey']
 
+if ($adminkey!='' && email!='' && password!=''){
+	header("Location:admin.html");
+}
+else{
+	header("Location:index.html");
+}
+/*
 //mysqli_connect() funciton opens a new conneciton to the MySQL server
 $conn = mysqli_connect("localhost", "root", "", "company");
 
 // SQL query to fetch information of registered users and finds user match
-$query = "SELECT username, password from login where username=? AND password=? LIMIT 1";
+$query = "SELECT username, password from login where username=? AND password=? AND adminkey=? LIMIT 1";
 
 // To protect MySQL injection for security purposes
 $stmt = $conn->prepare($query);
-$stmt->bind_param("ss", $username, $password);
+$stmt->bind_param("ss", $username, $password, $adminkey);
 $stmt->execute();
-$stmt->bind_result($username, $password);
+$stmt->bind_result($username, $password, $adminkey);
 $stmt->store_result();
 
+
+
+if ()
 if($stmt->fetch()) // fetching the contents of the row
 	{
 		$_SESSION['login_user'] = $username; // Initializing Session
@@ -34,6 +45,7 @@ else {
 		$error = "Username or Password is invalid";
 	}
 mysqli_close($conn); // Clossing Connection
+}*/
 }
 }
 ?>
